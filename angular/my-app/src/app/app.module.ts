132df191967f267fa294753042b89a93548ee095
gzq <571051761@qq.com>
2018-10-18 16:29:54 +0800

    2018-10-18- 16:29

--- angular/my-app/src/app/app.module.ts
+++ angular/my-app/src/app/app.module.ts
@@ -4,19 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MtComponent } from './mt/mt.component';
-import { MatTableModule, MatPaginatorModule, MatSortModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule } from '@angular/material';
+import {
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatIconModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule
+} from '@angular/material';
 import { MdComponent } from './md/md.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MnComponent } from './mn/mn.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MtComponent,
-    MdComponent,
-    MnComponent
-  ],
+  declarations: [AppComponent, MtComponent, MdComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -37,4 +43,4 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
